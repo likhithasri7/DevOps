756a571c21a0d4246bcf9a90ec7776fdc84a19b6
Madhav <madhavkolasani@gmail.com>
2025-07-07 11:54:21 +0530

    Initial

--- process.php
+++ process.php
@@ -1,16 +1,17 @@
 <?php
-if("POST"){
-    $fname=filter_var($_POST["fname"],FILTER_SANITIZE_STRING);
-    $lname=filter_var($_POST["lname"],FILTER_SANITIZE_STRING);
-    $email=filter_var($_POST["email"],FILTER_VALIDATE_EMAIL);
-    $mobile=filter_var($_POST["mobile"],FILTER_SANITIZE_STRING);
-}
-if(empty($fname)||empty($lname)||empty($email)||empty($mobile)){
-    echo("Fill required fields");
-}else{
-    echo "Registration Successfull<br>";
-    echo "Name: $fname $lname<br>";
-    echo "Email : $email<br>";
-    echo "Mobile No.: $mobile<br>";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fname = filter_var($_POST["fname"], FILTER_SANITIZE_STRING);
+    $lname = filter_var($_POST["lname"], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+    $mobile = filter_var($_POST["mobile"], FILTER_SANITIZE_STRING);
+
+    if (empty($fname) || empty($lname) || empty($email) || empty($mobile)) {
+        echo "Fill all required fields";
+    } else {
+        echo "Registration Successful<br>";
+        echo "Name: $fname $lname<br>";
+        echo "Email: $email<br>";
+        echo "Mobile No.: $mobile<br>";
+    }
 }
 ?>
